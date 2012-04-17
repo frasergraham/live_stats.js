87ead02a64a2757459d404652c339611f83c1da1
Fraser Graham <frasergraham@me.com>
2012-04-16 20:51:01 -0600

    data driven data source

--- ws_test.js
+++ ws_test.js
@@ -1,28 +1,31 @@
 
+var fs = require('fs')
+var data = require("./source.json");
 var WebSocketServer = require('ws').Server
 
 wss = new WebSocketServer({port: 8080});
 
-wss.on('connection', function(ws) {
+watch = fs.watchFile("./source.json", function(event){
+	fs.readFile("./source.json", function(err, file_contents){
+		if(err) throw err;
+		data = JSON.parse(file_contents);
+		console.log(data)
+	});
+});
 
-	
+console.log(data);
+
+wss.on('connection', function(ws) {
 
 	var interval = setInterval(function(){
 	
-		var tmp = ['fps',
-              'ms/f',
-              'mem',
-              'tex. mem',
-              'mesh mem',
-              'draw calls',
-              "junk"];
-
-		tmp = tmp.map(function(e){ 
+		tmp = data.map(function(e){ 
 						return {name : e, 
 								value: Math.floor(Math.random() * 100)
 								}
 							});
 
+		console.log(tmp);
 		ws.send(JSON.stringify(tmp));
 	}, 1000);
 
