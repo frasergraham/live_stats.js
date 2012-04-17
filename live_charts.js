60f638941eda8f2b737b1719cf91bb10c87ed9e5
Fraser Graham <frasergraham@me.com>
2012-04-16 20:50:32 -0600

    Dynamic add/remove of data and less duplication of code

--- live_charts.js
+++ live_charts.js
@@ -7,12 +7,42 @@ var live_charts = function(my) {
   // Charts are created with a server and a place in the DOM to put them
   // All the contents of the chart will come from the server
   // A chart will manage it's own 
-  my.new_bar_chart = function(websocket_server, selector){
+  my.new_bar_chart = function(websocket_server, selector, width, height){
     
+    var width = typeof width !== 'undefined' ? width : 400;
+    var height = typeof height !== 'undefined' ? height : 400;
+
+    var margin_padding = 60;
+
     var data;
     var chart, x, y;
     var connection;
     var ping;
+
+    chart = d3.select(selector)
+     .append("svg:svg")
+     .attr("class", "chart")
+     .attr("width", width)
+     .attr("height", height)
+     .append("g")        // this is a group tag
+     .attr("transform", "translate(60,0)"); // translate the group together
+
+    x = d3.scale.linear()
+       .domain([0, 100])
+       .range([0, width - margin_padding - 10]);
+     
+    y = d3.scale.ordinal()
+        .rangeBands([0, height]);
+
+    chart.selectAll("line")
+        .data(x.ticks(10))
+        .enter().append("line")
+        .attr("x1", x)
+        .attr("x2", x)
+        .attr("y1", 0)
+        .attr("y2", height)
+        .style("stroke", "#ccc");
+
 
     var establish_connection = function(){
       connection = new WebSocket(websocket_server, null);
@@ -31,80 +61,71 @@ var live_charts = function(my) {
 
       connection.onmessage = function (e) {
        
-        if  (!data){
-          // we're getting new data for the first time
-          data = JSON.parse(e.data);
-          
-          chart = d3.select(selector)
-             .append("svg:svg")
-             .attr("class", "chart")
-             .attr("width", 700)
-             .attr("height", 30 * data.length + 20)
-             .append("g")        // this is a group tag
-             .attr("transform", "translate(60,0)"); // translate the group together
-           
-          x = d3.scale.linear()
-             .domain([0, 100])
-             .range([0, 500]);
-           
-          y = d3.scale.ordinal()
-              .domain(data.map(function (d){ return d.name;}))
-              .rangeBands([0, 30 * data.length]);
-
-
-          chart.selectAll("line")
-              .data(x.ticks(10))
-              .enter().append("line")
-              .attr("x1", x)
-              .attr("x2", x)
-              .attr("y1", 0)
-              .attr("y2", 30 * data.length)
-              .style("stroke", "#ccc");
-             
-          chart.selectAll("rect")
-              .data(data)
-              .enter().append("svg:rect")
+        var build_node = function(d){
+          return d.append("svg:rect")
               .attr("y", function(d,i){ return y(d.name);})
               .attr("width", function(d,i){ return x(d.value);})
               .attr("height", y.rangeBand());
-          
-          chart.selectAll("text")
-              .data(data)
-              .enter().append("svg:text")
+        };
+
+        var build_label = function(d){
+            return d.append("svg:text")
               .attr("x", function(d,i){ return x(d.value);})
               .attr("y", function(d) { return y(d.name) + y.rangeBand() / 2; })
               .attr("dx", -3) // padding-right
               .attr("dy", ".35em") // vertical-align: middle
               .attr("text-anchor", "end") // text-align: right
+              .attr("class", "label")
               .text(function(d,i){return String(d.value)});
+        };
 
-          chart.selectAll("text.yAxis")
-              .data(data)
-              .enter().append("svg:text")
-              .attr("x", -60)
+        var build_axis = function(d){
+          return d.append("svg:text")
+              .attr("x", -margin_padding)
               .attr("y", function(d) { return y(d.name) + y.rangeBand() / 2; })
               .attr("dx", 0) // padding-right
               .attr("dy", ".35em") // vertical-align: middle
+              .attr("class", "yAxis")
               .text(function(d,i){return String(d.name)});
+        };
 
-        }
-        else{
+    
+        data = JSON.parse(e.data);
+        
+        var bars = chart.selectAll("rect").data(data);
+        var labels = chart.selectAll("text.label").data(data);
+        var axis = chart.selectAll("text.yAxis").data(data);
 
-          data = JSON.parse(e.data);
-            
-          chart.selectAll("rect")
-            .data(data)
-            .transition()
-            .duration(500)
-            .attr("width", function(d){return x(d.value)});          
+        y.domain(data.map(function (d){ return d.name;}));
 
-          chart.selectAll("text")
-            .data(data)
-            .transition()
-            .duration(500)
-            .text(function(d){return String(d.value)})
-            .attr("x", function(d){return x(d.value)});
-        }
+        // BARS
+        bars.transition()
+          .duration(500)
+          .attr("width", function(d){return x(d.value)})
+          .attr("y", function(d,i){ return y(d.name);})
+          .attr("height", y.rangeBand());          
+
+        build_node(bars.enter());
+        bars.exit().remove();
+
+        // LABELS
+        labels.transition()
+          .duration(500)
+          .text(function(d){return String(d.value)})
+          .attr("y", function(d,i){ return y(d.name) + y.rangeBand() / 2;})
+          .attr("x", function(d){return x(d.value)});
+
+        build_label(labels.enter());
+        labels.exit().remove();
+
+        // AXIS
+        axis.transition()
+          .duration(500)
+          .attr("y", function(d,i){ return y(d.name) + y.rangeBand() / 2;})
+
+        build_axis(axis.enter());
+        axis.exit().remove();
+
       };  
 
       return connection;
