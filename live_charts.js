11eb09f3e29720e7c943b2d75ddda1ec6f3b052c
Fraser Graham <frasergraham@me.com>
2012-04-16 21:38:02 -0600

    broke the websocket connection our from the chart, requiring a connect, then a new_chart call. Now only one connection drives many charts

--- live_charts.js
+++ live_charts.js
@@ -4,11 +4,56 @@
 
 var live_charts = function(my) {
   
+  // Store which data sources refresh which graphs
+  my.source_mappings = {};
+
+  my.connect_to_data_source = function(websocket_server){
+      my.source_mappings[websocket_server] = {};
+      
+      connection = new WebSocket(websocket_server, null);
+
+      connection.onopen = function(){
+        console.log("Connected to " + websocket_server);
+        clearInterval(ping);
+      };
+
+      connection.onclose = function(){
+        console.log("Disconnected from " + websocket_server);
+        setTimeout(function(){
+          establish_connection();
+        }, 5000);
+      };
+
+      connection.onmessage = function (e) {
+        new_data = JSON.parse(e.data);
+        for (set in new_data){
+          if (typeof my.source_mappings[websocket_server][set] !== 'undefined'){
+            my.source_mappings[websocket_server][set](new_data[set]);
+          }
+        }
+      };
+  };
+
+  my.register_data_source = function(websocket_server, source_set, draw_callback){
+
+    if (typeof my.source_mappings[websocket_server] === 'undefined'){
+      throw websocket_server + " is not connected, call connect_to_data_source() first";
+      return;
+    }
+
+    my.source_mappings[websocket_server][source_set] = draw_callback;
+  }
+
   // Charts are created with a server and a place in the DOM to put them
   // All the contents of the chart will come from the server
   // A chart will manage it's own 
-  my.new_bar_chart = function(websocket_server, selector, width, height){
+  my.new_bar_chart = function(websocket_server, selector, source_set, width, height){
     
+    if (typeof my.source_mappings[websocket_server] === 'undefined'){
+      throw websocket_server + " is not connected, call connect_to_data_source() first";
+      return;
+    }
+
     var width = typeof width !== 'undefined' ? width : 400;
     var height = typeof height !== 'undefined' ? height : 400;
 
@@ -44,96 +89,76 @@ var live_charts = function(my) {
         .style("stroke", "#ccc");
 
 
-    var establish_connection = function(){
-      connection = new WebSocket(websocket_server, null);
-      
-      connection.onopen = function(){
-        console.log("Connected to " + websocket_server);
-        clearInterval(ping);
-      };
-
-      connection.onclose = function(){
-        console.log("Disconnected from " + websocket_server);
-        setTimeout(function(){
-          establish_connection();
-        }, 5000);
-      };
-
-      connection.onmessage = function (e) {
+    var draw = function (data_src) {
        
-        var build_node = function(d){
-          return d.append("svg:rect")
-              .attr("y", function(d,i){ return y(d.name);})
-              .attr("width", function(d,i){ return x(d.value);})
-              .attr("height", y.rangeBand());
-        };
+      var build_node = function(d){
+        return d.append("svg:rect")
+            .attr("y", function(d,i){ return y(d.name);})
+            .attr("width", function(d,i){ return x(d.value);})
+            .attr("height", y.rangeBand());
+      };
 
-        var build_label = function(d){
-            return d.append("svg:text")
-              .attr("x", function(d,i){ return x(d.value);})
-              .attr("y", function(d) { return y(d.name) + y.rangeBand() / 2; })
-              .attr("dx", -3) // padding-right
-              .attr("dy", ".35em") // vertical-align: middle
-              .attr("text-anchor", "end") // text-align: right
-              .attr("class", "label")
-              .text(function(d,i){return String(d.value)});
-        };
-
-        var build_axis = function(d){
+      var build_label = function(d){
           return d.append("svg:text")
-              .attr("x", -margin_padding)
-              .attr("y", function(d) { return y(d.name) + y.rangeBand() / 2; })
-              .attr("dx", 0) // padding-right
-              .attr("dy", ".35em") // vertical-align: middle
-              .attr("class", "yAxis")
-              .text(function(d,i){return String(d.name)});
-        };
+            .attr("x", function(d,i){ return x(d.value);})
+            .attr("y", function(d) { return y(d.name) + y.rangeBand() / 2; })
+            .attr("dx", -3) // padding-right
+            .attr("dy", ".35em") // vertical-align: middle
+            .attr("text-anchor", "end") // text-align: right
+            .attr("class", "label")
+            .text(function(d,i){return String(d.value)});
+      };
 
-    
-        data = JSON.parse(e.data);
-        
-        var bars = chart.selectAll("rect").data(data);
-        var labels = chart.selectAll("text.label").data(data);
-        var axis = chart.selectAll("text.yAxis").data(data);
+      var build_axis = function(d){
+        return d.append("svg:text")
+            .attr("x", -margin_padding)
+            .attr("y", function(d) { return y(d.name) + y.rangeBand() / 2; })
+            .attr("dx", 0) // padding-right
+            .attr("dy", ".35em") // vertical-align: middle
+            .attr("class", "yAxis")
+            .text(function(d,i){return String(d.name)});
+      };
 
-        y.domain(data.map(function (d){ return d.name;}));
+      data = data_src;
 
-        // BARS
-        bars.transition()
-          .duration(500)
-          .attr("width", function(d){return x(d.value)})
-          .attr("y", function(d,i){ return y(d.name);})
-          .attr("height", y.rangeBand());          
+      var bars = chart.selectAll("rect").data(data);
+      var labels = chart.selectAll("text.label").data(data);
+      var axis = chart.selectAll("text.yAxis").data(data);
 
-        build_node(bars.enter());
-        bars.exit().remove();
+      y.domain(data.map(function (d){ return d.name;}));
 
-        // LABELS
-        labels.transition()
-          .duration(500)
-          .text(function(d){return String(d.value)})
-          .attr("y", function(d,i){ return y(d.name) + y.rangeBand() / 2;})
-          .attr("x", function(d){return x(d.value)});
+      // BARS
+      bars.transition()
+        .duration(500)
+        .attr("width", function(d){return x(d.value)})
+        .attr("y", function(d,i){ return y(d.name);})
+        .attr("height", y.rangeBand());          
 
-        build_label(labels.enter());
-        labels.exit().remove();
+      build_node(bars.enter());
+      bars.exit().remove();
 
-        // AXIS
-        axis.transition()
-          .duration(500)
-          .attr("y", function(d,i){ return y(d.name) + y.rangeBand() / 2;})
+      // LABELS
+      labels.transition()
+        .duration(500)
+        .text(function(d){return String(d.value)})
+        .attr("y", function(d,i){ return y(d.name) + y.rangeBand() / 2;})
+        .attr("x", function(d){return x(d.value)});
 
-        build_axis(axis.enter());
-        axis.exit().remove();
+      build_label(labels.enter());
+      labels.exit().remove();
 
-      };  
+      // AXIS
+      axis.transition()
+        .duration(500)
+        .attr("y", function(d,i){ return y(d.name) + y.rangeBand() / 2;})
 
-      return connection;
-    };
+      build_axis(axis.enter());
+      axis.exit().remove();
 
-    establish_connection();
-  }
+    };  
 
+    my.register_data_source(websocket_server, source_set, draw);
+  };
 
   return my;
 
