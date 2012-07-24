904bdbd226d9e6ffb4fbf7610d950b69e07613df
Fraser Graham <frasergraham@me.com>
2012-07-24 15:07:54 -0600

    generate some sample data to better test the static charts

---
+++ client/generate_sample_data.js
@@ -0,0 +1,18 @@
+var fs = require('fs');
+var os = require('os');
+
+fs.open("./test_data_generated.json", "w", 0666, function(err, fd){
+    var data = [];
+
+    for (var i = 0; i < 10; i++){
+        data[i] = [];
+        for (var p = 0; p < 50; p++){
+            data[i].push({ "name" : "test_" + i, "value": Math.floor(Math.random() * 200)});
+        }
+    }
+
+    var json_data = JSON.stringify(data);
+    console.log(json_data);
+    fs.write(fd, json_data, 0, json_data.length);
+
+});
